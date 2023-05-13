c83b29efafd0c7004231625b20881b5126ef287b
Grammeri <89168093+Grammeri@users.noreply.github.com>
2023-05-13 14:14:02 -0500

    Two links on projects

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from './../common/styles/Container.module.css';
 import myPhoto from '../assets/image/4Dima.jpg';
@@ -7,89 +7,18 @@ import Particles from "react-particles";
 import { Container, Engine } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
 import ReactTypingEffect from "react-typing-effect";
-import Fade from "react-awesome-reveal"
-import { Tilt } from 'react-tilt'
-
-/*
-const particlesOptions = {
-    fpsLimit: 120,
-    interactivity: {
-        events: {
-            onClick: {
-                enable: true,
-                mode: "push",
-            },
-            onHover: {
-                enable: true,
-                mode: "repulse",
-            },
-            resize: true,
-        },
-        modes: {
-            push: {
-                quantity: 4,
-            },
-            repulse: {
-                distance: 200,
-                duration: 0.4,
-            },
-        },
-    },
-    particles: {
-        color: {
-            value: "#ffffff",
-        },
-        links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-        },
-        collisions: {
-            enable: true,
-        },
-        move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-                default: "bounce",
-            },
-            random: false,
-            speed: 1,
-            straight: false,
-        },
-        number: {
-            density: {
-                enable: true,
-                area: 800,
-            },
-            value: 80,
-        },
-        opacity: {
-            value: 0.5,
-        },
-        shape: {
-            type: "circle",
-        },
-        size: {
-            value: {min: 1, max: 5},
-        },
-    },
-    detectRetina: true,
-}
-*/
+import Fade from "react-awesome-reveal";
+import { Tilt } from 'react-tilt';
 
 export const Main = () => {
-
-    const particlesInit = useCallback(async (engine: Engine) => {
+    const particlesInit = async (engine: Engine) => {
         console.log(engine);
         await loadFull(engine);
-    }, []);
+    };
 
-    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+    const particlesLoaded = async (container: Container | undefined) => {
         await console.log(container);
-    }, []);
+    };
 
     const dimaPhoto = {
         backgroundImage: `url(${myPhoto})`,
@@ -103,34 +32,29 @@ export const Main = () => {
 
     return (
         <div id="Main" className={style.mainBlock}>
-            {/*<Particles
-                className={style}
+            <Particles
+                className={style.particles}
                 id="tsparticles"
                 init={particlesInit}
                 loaded={particlesLoaded}
-                params={particlesOptions}
-                options={{ fullScreen: { enable: false } }}
-            />*/}
+            />
+
             <Fade>
-                <div className={styleContainer.container}>
-
+                <div className={`${styleContainer.container} ${style.mainContainer}`}>
                     <div className={style.greeting}>
-
                         <h>Hi there!</h>
                         <h1>I am Dmitry Nikolayev</h1>
-
                         <h1>Frontend React Developer</h1>
                         <ReactTypingEffect
                             text={"Frontend React Developer " + "from Houston, Texas"}
                         />
-
                     </div>
+
                     <Tilt options={{}}>
                         <div className={style.photo}>
                             <MyPhoto style={dimaPhoto} />
                         </div>
                     </Tilt>
-
                 </div>
             </Fade>
         </div>
--- src/projects/MyProjects.tsx
+++ src/projects/MyProjects.tsx
@@ -1,73 +1,82 @@
-import React from 'react'
-import style from './MyProjects.module.scss'
-import {MyProject} from './project/Myproject';
+import React, { useState } from 'react';
+import style from './MyProjects.module.scss';
+import { MyProject } from './project/Myproject';
 import styleContainer from '../common/styles/Container.module.css';
-import {Title} from '../common/Components/title/Title';
+import { Title } from '../common/Components/title/Title';
 import socialNetWorkImg from './../assets/image/SocialNetwork.jpg';
 import todoImg from './../assets/image/Todolist.jpg';
-import startUpImg from "./../assets/image/StartUp.jpg"
-import flashCardsImg from "./../assets/image/FlashCards.jpg"
-
+import startUpImg from "./../assets/image/StartUp.jpg";
+import flashCardsImg from "./../assets/image/FlashCards.jpg";
 
 export const MyProjects = () => {
+    const projects = [
+        {
+            title: "Todolist",
+            image: todoImg,
+            appUrl: "https://grammeri.github.io/Todolist/",
+            codeUrl: "https://github.com/grammeri/Todolist"
+        },
+        {
+            title: "Social Network",
+            image: socialNetWorkImg,
+            appUrl: "https://grammeri.github.io/Samurai_23/",
+            codeUrl: "https://github.com/grammeri/Samurai_23"
+        },
+        {
+            title: "Start Up",
+            image: startUpImg,
+            appUrl: "https://silevans-a2m9qgc35-i6169696-gmailcom.vercel.app/",
+            codeUrl: "https://github.com/Dmitry-Klyuev/silevans"
+        },
+        {
+            title: "FlashCards",
+            image: flashCardsImg,
+            appUrl: "https://flashcards-bzwnji4ny-horobryh22.vercel.app/packs",
+            codeUrl: "https://github.com/horobryh22/flashcards"
+        }
+    ];
 
-    const jumpToToDo = () => {
-        const todoUrl = 'https://grammeri.github.io/Todolist/';
-        window.open(todoUrl, '_blank');
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const openApp = (appUrl) => {
+        window.open(appUrl, '_blank');
     };
 
-    const jumpToSocialNetwork = () => {
-        const todoUrl = 'https://grammeri.github.io/Samurai_23/';
-        window.open(todoUrl, '_blank');
+    const openCode = (codeUrl) => {
+        window.open(codeUrl, '_blank');
     };
 
-    const jumpToStartUp = () => {
-        const todoUrl = 'https://silevans-a2m9qgc35-i6169696-gmailcom.vercel.app/';
-        window.open(todoUrl, '_blank');
-    };
-    const jumpToFlashCards = () => {
-        const todoUrl = 'https://flashcards-bzwnji4ny-horobryh22.vercel.app/packs';
-        window.open(todoUrl, '_blank');
+    const handleMouseEnter = (index) => {
+        setActiveIndex(index);
     };
 
-    const social = {
-        backgroundImage: `url(${socialNetWorkImg})`,
-    };
-    const todolist = {
-        backgroundImage: `url(${todoImg})`,
-    };
-
-    const startUp = {
-        backgroundImage: `url(${startUpImg})`,
-    };
-
-    const flashCards = {
-        backgroundImage: `url(${flashCardsImg})`,
+    const handleMouseLeave = () => {
+        setActiveIndex(null);
     };
 
     return (
         <div id="MyProjects" className={style.myProjectsBlock}>
             <div className={`${styleContainer.container} ${style.myProjectsContainer}`}>
-                <Title header={"My Projects"}/>
+                <Title header={"My Projects"} />
                 <div className={style.projects}>
-
-                    <div onClick={jumpToToDo}>
-                        <MyProject style={todolist} projectTitle={"Todolist"}/>
-                    </div>
-
-                    <div onClick={jumpToSocialNetwork}>
-                        <MyProject style={social} projectTitle={"Social Network"}/>
-                    </div>
-
-                    <div onClick={jumpToStartUp}>
-                        <MyProject style={startUp} projectTitle={"Start Up"}/>
-                    </div>
-                    <div onClick={jumpToFlashCards}>
-                        <MyProject style={flashCards} projectTitle={"FlashCards"}/>
-                    </div>
+                    {projects.map((project, index) => (
+                        <div
+                            key={index}
+                            className={style.projectItem}
+                            onMouseEnter={() => handleMouseEnter(index)}
+                            onMouseLeave={handleMouseLeave}
+                        >
+                            <MyProject style={{ backgroundImage: `url(${project.image})` }} projectTitle={project.title} />
+                            {activeIndex === index && (
+                                <div className={style.buttonsContainer}>
+                                    <button className={style.appButton} onClick={() => openApp(project.appUrl)}>Open Application</button>
+                                    <button className={style.codeButton} onClick={() => openCode(project.codeUrl)}>Open GitHub</button>
+                                </div>
+                            )}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-    )
-}
-
+    );
+};
