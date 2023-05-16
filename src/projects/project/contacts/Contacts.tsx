25f9cae8c5d1e80ade766895a6bc9c289b80ea7e
Grammeri <89168093+Grammeri@users.noreply.github.com>
2023-05-16 12:57:23 -0500

    Email form fixed

--- src/projects/project/contacts/Contacts.tsx
+++ src/projects/project/contacts/Contacts.tsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import {useForm} from 'react-form';
+import React, { useEffect, useRef } from 'react';
+import emailjs from 'emailjs-com';
 import style from './Contacts.module.scss';
 import {Title} from './../../../../src/common/Components/title/Title';
-import axios from "axios";
 
 export const Contacts = () => {
-    const handleSubmit = async (values) => {
-        // Handle form submission logic here
-        // For example, send data to a server or show a success message
-        console.log(values);
-        try {
-            const response = await axios.post('http://localhost:3010/sendMessage', values);
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    const form:any = useRef();
 
-    const {
-        Form,
-        meta: { isSubmitting },
-    } = useForm({ onSubmit: handleSubmit });
+    useEffect(() => {
+        emailjs.init('SW5P4TVHUa0JytrLA');
+
+        form.current.addEventListener('submit', function(event) {
+            event.preventDefault();
+            // these IDs from the previous steps
+            emailjs.sendForm('service_lal0mp4', 'contact_form', this)
+                .then(function() {
+                    console.log('SUCCESS!');
+                    window.alert('Email successfully sent!');
+                }, function(error) {
+                    console.log('FAILED...', error);
+                });
+        });
+    }, []);
 
     return (
         <div id="Contacts" className={style.contactsBlock}>
             <div id="Contacts" className={`${style.contactsContainer}`}>
                 <Title header={'Contacts'} />
 
-                <Form className={style.contactForm}>
+                <form ref={form} className={style.contactForm}>
                     <label htmlFor="name">Name*</label>
-                    <input type="text" name="name" id="name" required />
+                    <input type="text" name="user_name" id="name" required placeholder={"Your name"}/>
                     <label htmlFor="email">Email*</label>
-                    <input type="email" name="email" id="email" required />
+                    <input type="email" name="user_email" id="email" required placeholder={"Your e-mail"}/>
                     <label htmlFor="message">Message*</label>
-                    <textarea name="message" id="message" required />
-                    <button type="submit" disabled={isSubmitting} className={style.submitBtn}>
+                    <textarea name="message" id="message" required placeholder={"Your message"}/>
+                    <button type="submit" className={style.submitBtn}>
                         Send
                     </button>
                     <div>
-                        Email and message form is temporarily not working, please use @ link below
+                        I am a working form configured with emailjs, please send your messages to Dmitry!
                     </div>
-                </Form>
+                </form>
             </div>
         </div>
     );
